50bb450e7716eb38b0ec0aa655a6a0a9151ca94f
yuvrajchawala <96770264+yuvrajchawala@users.noreply.github.com>
2022-01-09 20:16:41 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,30 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\neeru\Desktop\assignment 1.py ===============
+ENTER FIRST NUMBER: 85
+ENTER SECOND NUMBER: 48
+ENTER THIRD NUMBER: 76
+The average of three numbers is:  69.66666666666667
+********************************************************
+ENTER YOUR INCOME: 100000
+ENTER NUMBER OF DEPENDENTS: 8
+Your taxable income is:  66000.0
+Your tax amount is:  13200.0
+**********************************************************
+ENTER YOUR STUDENT ID: 21104107
+ENTER YOUR NAME: Yuvraj
+ENTER F FOR FEMALE AND M FOR MALE AND U FOR UNKNOWN: m
+ENTER COURSE NAME: EE
+ENTER YOUR CGPA: 9.8
+[21104107, 'Yuvraj', 'm', 'EE', 9.8]
+**************************************************************
+ENTER FIRST STUDENT MARKS: 95
+ENTER SECOND STUDENT MARKS: 84
+ENTER THIRD STUDENT MARKS: 96
+ENTER FOURTH STUDENT MARKS: 92
+ENTER FIFTH STUDENT MARKS: 81
+[81, 84, 92, 95, 96]
+*****************************************************************
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']

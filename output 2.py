943839b2fdfc894b9ac614fec1258f094c2136c4
yuvrajchawala <96770264+yuvrajchawala@users.noreply.github.com>
2022-01-24 18:46:47 +0530

    Add files via upload

---
+++ output 2.py
@@ -0,0 +1,52 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\neeru\Desktop\python assignments\assignment 2.py ======
+Question 1
+
+The length of given string is: 35
+Reversed string is: egaugnal evitisnes esac a si nohtyP
+New stored string is:  a case sensitive
+New string is: Python is object oriented language
+The index of a is: 10
+Pythonisacasesensitivelanguage
+***********************************************************************************
+
+Qustion 2
+
+Hey YUVRAJ CHAWALA ,Here!
+My SID is 21104107
+I am from ELECTRICAL department and my CGPA is 9.9
+***********************************************************************************
+
+Qustion 3
+
+a&b: 8
+a|b: 58
+a^b: 50
+a left shifted by 2 bits is: 224
+b left shifted by 2 bits is: 40
+a right shifted by 2 bits is: 14
+b right shifted by 4 bits is: 0
+***********************************************************************************
+
+Qustion 4
+
+Enter first number:7
+Enter second number:6
+Enter third number:2
+The largest number is: 7
+***********************************************************************************
+
+Qustion 5
+
+Enter your string:My name is YUVRAJ
+YES,'name' is present
+***********************************************************************************
+
+Qustion 6
+
+Enter first side:8
+Enter second side:7
+Enter third side:12
+Triangle is possible

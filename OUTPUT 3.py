c541b62646cd5dc9b11c61360ac6cb4503ee8f5f
yuvrajchawala <96770264+yuvrajchawala@users.noreply.github.com>
2022-02-14 22:31:26 +0530

    Add files via upload

---
+++ OUTPUT 3.py
@@ -0,0 +1,89 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\neeru\Desktop\python assignments\assignment 3.py ======
+Question 1
+
+Enter your string: Yuvraj is very very good boy
+The occurance of Yuvraj  is: 1
+The occurance of is  is: 1
+The occurance of very  is: 2
+The occurance of very  is: 2
+The occurance of good  is: 1
+The occurance of boy  is: 1
+
+***********************************************************************************************************
+
+Question 2
+
+ENTER THE DAY [1-31]:28
+ENTER THE MONTH[1-12]:2
+ENTER THE YEAR[1800-2025]:1999
+THE NEXT DATE IS 1 3 1999
+
+***********************************************************************************************************
+
+Question 3
+
+ENTER NO. OF ENTERIES YOU WANT TO ENTER:3
+Enter the entry:3
+Enter the entry:9
+Enter the entry:10
+Input list is: [3, 9, 10]
+Output list is: [(3, 9), (9, 81), (10, 100)]
+
+***********************************************************************************************************
+
+Question 4
+
+Enter grade of student between 4 and 10:9
+Your Grade is 'A' and EXCELLENT Performance
+
+***********************************************************************************************************
+
+Question 5
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+***********************************************************************************************************
+
+Question 6
+
+Enter 'Y' if you want to give entry and 'N' to finish program:Y
+ENTER SID:21104107
+Enter name of student:YUVRAJ
+Enter 'Y' if you want to give another entry and 'N' to finish program:Y
+ENTER SID:21104096
+Enter name of student:KUNAL
+Enter 'Y' if you want to give another entry and 'N' to finish program:Y
+ENTER SID:21104108
+Enter name of student:VISHNU
+Enter 'Y' if you want to give another entry and 'N' to finish program:N
+The final dictionary is: {21104107: 'YUVRAJ', 21104096: 'KUNAL', 21104108: 'VISHNU'}
+The sorted dictionary by 'NAMES' is: [(21104096, 'KUNAL'), (21104108, 'VISHNU'), (21104107, 'YUVRAJ')]
+The sorted dictionary by 'SID' is: [(21104096, 'KUNAL'), (21104107, 'YUVRAJ'), (21104108, 'VISHNU')]
+Enter SID you want to search:21104107
+The student name is: YUVRAJ
+
+***********************************************************************************************************
+
+Question 7
+
+Enter the no. of entries:6
+The Fibonacci series is: [0, 1, 1, 2, 3, 5]
+AVERAGE= 1.0
+
+***********************************************************************************************************
+
+Question 8
+
+The answer of 8.a: {1, 3, 5, 6, 8}
+The answer of 8.b: {17, 3, 6, 8, 9, 13}
+The answer of 8.c: {1, 2, 4, 5}
+The answer of 8.d: {6, 7, 8, 9, 10}
+The answer of 8.e: {10, 7}

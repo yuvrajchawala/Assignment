97c57efae30aea2dff3a80de5f9871dd5c9a574f
yuvrajchawala <96770264+yuvrajchawala@users.noreply.github.com>
2022-03-01 13:30:37 +0530

    Add files via upload

---
+++ OUTPUT 4.py
@@ -0,0 +1,81 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====== RESTART: C:\Users\neeru\Desktop\python assignments\ASSIGNMENT 4.py ======
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+
+********************************************************************************************************************************
+
+
+Question 2
+Enter the no. rows:4
+
+USING RECURSION
+
+   1 
+  1 1 
+ 1 2 1 
+1 3 3 1 
+
+USING ITERATION
+
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+
+********************************************************************************************************************************
+
+
+Question 3
+Enter the first integer:65
+Enter the second integer:7
+The quotient is: 9
+The modulous is: 2
+The function is callable
+No zeroes are present
+The new list is: [65, 7, 9, 2, 4, 5, 6]
+The filtered list: [65, 7, 9, 5, 6]
+The new set is: {65, 2, 4, 5, 6, 7, 9}
+The frozen set is: frozenset({65, 2, 4, 5, 6, 7, 9})
+The maximum value is: 65
+The hash value is: 65
+
+********************************************************************************************************************************
+
+
+Question 4
+Initiator called
+The student name is Yuvraj
+The student ROLL NO. is 21104107
+The object is destroyed
+
+********************************************************************************************************************************
+
+
+Question 5
+++++++++++++++++Initiator called+++++++++++++++++++++++
+The original salary of employee is 40000
+The Employee name is Mehak
+The updated Employee salary is 70000
+++++++++++++++++Initiator called+++++++++++++++++++++++
+The Employee name is Ashok
+The Employee salary is 50000
+++++++++++++++++Initiator called+++++++++++++++++++++++
+The Employee name is Viren
+The Employee salary is 60000
+++++++++++++++++++The object is destroyed+++++++++++++++++++
+
+********************************************************************************************************************************
+
+
+Question 6
+Enter the word:bat
+Enter the test word:tab
+You have real friendship
